7f6c9108d2f03d4ea05d163d3a0768c464369eb5
MajoJojo <garces-1992@hotmail.com>
2023-07-16 03:52:50 -0500

    modo de color changes js

--- index.js
+++ index.js
@@ -28,19 +28,36 @@ textMenu.addEventListener("click", ()=>{
 //modo claro
 const colorMode = document.querySelector(".light-icon")
 
+function toggleTittle() {
+    if (colorMode.innerHTML === "Modo claro") {
+        colorMode.innerHTML = "Modo oscuro";
+    } else {
+        colorMode.innerHTML = "Modo claro";
+    }
+  }
+
 colorMode.addEventListener("click", () => {
+
     const segmentColor = document.querySelector(".second-segment").classList.toggle("second-segment-light")
     const headerColor = document.querySelector(".header").classList.toggle("background-light")
     const headerIconsColor = document.querySelector(".header-buttons-list").classList.toggle("header-buttons-list-light")
     const bodyColor = document.querySelector(".body").classList.toggle("body-light")
     const asideTextColor = document.querySelector(".text-edition").classList.toggle("aside-light")
     const asideImageColor = document.querySelector(".mode-edition").classList.toggle("aside-light")
-
-
     const inputColorLight = document.getElementById("url-field").classList.toggle("background-light")
     const inputTopTextLight = document.querySelector(".text-field-top").classList.toggle("background-light")
     const inputBottomTextLight = document.querySelector(".text-field-bottom").classList.toggle("background-light")
-
+    const selectMode = document.getElementById("select-mode").classList.toggle("background-light")
+    const selectFontFamily = document.getElementById("select-font-family").classList.toggle("background-light")
+    const paddingTextValue = document.getElementById("padding-text-value").classList.toggle("background-light")
+    const leftAlignText = document.getElementById("left-align-text").classList.toggle("background-light")
+    const centerAlignText = document.getElementById("center-align-text").classList.toggle("background-light")
+    const rightAlignText = document.getElementById("right-align-text").classList.toggle("background-light")
+    const textSizeInput = document.getElementById("text-size-input").classList.toggle("background-light")
+    const noneContourText = document.getElementById("none-contour-text").classList.toggle("background-light")
+    const ligthContourText = document.getElementById("ligth-contour-text").classList.toggle("background-light")
+    const darkContourText = document.getElementById("dark-contour-text").classList.toggle("background-light")
+    const selectLineHeight = document.getElementById("select-line-height").classList.toggle("background-light")
 
 
     
