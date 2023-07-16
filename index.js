f49c7dc279035789da28b45153fcf37ddfa72800
MajoJojo <garces-1992@hotmail.com>
2023-07-16 05:43:00 -0500

    filters chagnes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 
 //-------------------------------
 //buttons
-const mainImage = document.querySelector(".main-image")
 const imageMenu = document.querySelector(".image-icon")
 const textMenu = document.querySelector(".text-icon")
 const imageMenuButton= document.querySelector(".mode-edition")
@@ -16,13 +15,11 @@ imageMenu.addEventListener("click", ()=>{
     imageMenuButton.classList.toggle("display-visibility-image")
     imageMenuButton.style.zIndex = 15
     textMenuButton.style.zIndex = 8
-    console.log("aquitoyyo")
 })
 textMenu.addEventListener("click", ()=>{
     textMenuButton.classList.toggle("display-visibility-text")
     textMenuButton.style.zIndex = 15
     imageMenuButton.style.zIndex = 8
-    console.log("aquitoy")
 })
 //----------------------------
 //modo claro
@@ -37,7 +34,6 @@ function toggleTittle() {
   }
 
 colorMode.addEventListener("click", () => {
-
     const segmentColor = document.querySelector(".second-segment").classList.toggle("second-segment-light")
     const headerColor = document.querySelector(".header").classList.toggle("background-light")
     const headerIconsColor = document.querySelector(".header-buttons-list").classList.toggle("header-buttons-list-light")
@@ -57,58 +53,69 @@ colorMode.addEventListener("click", () => {
     const noneContourText = document.getElementById("none-contour-text").classList.toggle("background-light")
     const ligthContourText = document.getElementById("ligth-contour-text").classList.toggle("background-light")
     const darkContourText = document.getElementById("dark-contour-text").classList.toggle("background-light")
-    const selectLineHeight = document.getElementById("select-line-height").classList.toggle("background-light")
-
-
-    
-
-    
+    const selectLineHeight = document.getElementById("select-line-height").classList.toggle("background-light")  
 })
-//#select-mode,
-// #select-font-family,
-// #padding-text-value,
-// #left-align-text,
-// #center-align-text,
-// #right-align-text,
-// #text-size-input,
-// #none-contour-text,
-// #ligth-contour-text,
-// #dark-contour-text,
-// #select-line-height
-//image menu:
-//url image
+
+//url change
+const mainImage = document.querySelector(".main-image")
 const inputUrlMode = document.querySelector("#url-field")
 
 inputUrlMode.addEventListener ( "input", () => {
-    console.log ("funciona")
-    console.log (mainImage)
     mainImage.style.backgroundImage =  `url('${inputUrlMode.value}')`
 })
-
 //color selection text
 const colorBackgroundSelection = document.getElementById("background-text-color")
 const colorFontSelection = document.getElementById("color-text-color")
+
 const topFrameMeme = document.querySelector(".top-text")
 const bottomFrameMeme = document.querySelector(".bottom-text")
 
 const colorFontFrames = (e) => {
-    console.log("aquitoy", e)
     const colorFondoFont = e.target.value;
     topFrameMeme.style.color = colorFondoFont;
     bottomFrameMeme.style.color = colorFondoFont;
-    console.log("1")
 }
 
 colorFontSelection.addEventListener(`input`, (e) => colorFontFrames(e))
 
 const colorBackgroundFrames = (e) => {
-    console.log("aquitoy", e)
     const colorFondoBackground = e.target.value;
     topFrameMeme.style.backgroundColor = colorFondoBackground;
     bottomFrameMeme.style.backgroundColor = colorFondoBackground;
-    console.log("2")
 }
 
 colorBackgroundSelection.addEventListener(`input`, (e) => colorBackgroundFrames(e))
+// cambios de imagen de fondo/filtros
+//brillo
+const inputBrightness = document.querySelector("#brillo-range")
+const inputOpacity =document.querySelector("#opacidad-range")
+const inputContrast =document.querySelector("#contraste-range")
+const inputBlur =document.querySelector("#desenfoque-range")
+const inputGrayscale =document.querySelector("#escala-de-grises-range")
+const inputSepia =document.querySelector("#sepia-range")
+const inputHueRotate =document.querySelector("#hue-range")
+const inputSaturate =document.querySelector("#saturado-range")
+const inputInvert =document.querySelector("#negativo-range")
 
+const filtros = (e) => {
+    console.log(e.target.value)
+    mainImage.style.filter = `brightness(${inputBrightness.value}) opacity(${inputOpacity.value}) blur(${inputBlur.value}px) contrast(${inputContrast.value}%) grayscale(${inputGrayscale.value}%) hue-rotate(${inputHueRotate.value}deg) sepia(${inputSepia.value}%) saturate(${inputSaturate.value}%) invert(${inputInvert.value})`
+}
+
+inputBrightness.addEventListener(`input`, (e) => filtros(e))
+inputOpacity.addEventListener(`input`, (e) => filtros(e))
+inputContrast.addEventListener(`input`, (e) => filtros(e))
+inputBlur.addEventListener(`input`, (e) => filtros(e))
+inputGrayscale.addEventListener(`input`, (e) => filtros(e))
+inputSepia.addEventListener(`input`, (e) => filtros(e))
+inputHueRotate.addEventListener(`input`, (e) => filtros(e))
+inputSaturate.addEventListener(`input`, (e) => filtros(e))
+inputInvert.addEventListener(`input`, (e) => filtros(e))
+
+// const mainImage = document.querySelector(".main-image")
+// const inputUrlMode = document.querySelector("#url-field")
+
+// inputUrlMode.addEventListener ( "input", () => {
+//     mainImage.style.backgroundImage =  `url('${inputUrlMode.value}')`
+// })
 
